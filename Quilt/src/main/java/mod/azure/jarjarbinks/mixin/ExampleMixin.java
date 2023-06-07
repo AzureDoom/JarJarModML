f6594b4edef6f64cfab12f5dac6acbdf11c07e6c
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-06-07 18:29:39 -0400

    Removes Quilt

--- Quilt/src/main/java/mod/azure/jarjarbinks/mixin/ExampleMixin.java
+++
@@ -1,9 +0,0 @@
-package mod.azure.jarjarbinks.mixin;
-
-import net.minecraft.client.gui.screens.TitleScreen;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
-
-}
